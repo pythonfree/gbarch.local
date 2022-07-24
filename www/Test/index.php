919258eaab65daecfda0e064abcb529c8dbdc8a0
shahov.e <shahov.e@oneway.su>
2022-07-24 09:07:14 +0300

    Add tightenco.

--- www/Test/index.php
+++ www/Test/index.php
@@ -2,16 +2,53 @@
 
 namespace Test;
 
-function test()
-{
-    static $count = 0;
+include_once __DIR__ . '/../vendor/autoload.php';
 
-    $count++;
-    echo $count . PHP_EOL;
-    if ($count < 10) {
-        test();
-    }
-    $count--;
-    echo 'minus ' . $count . PHP_EOL;
-}
-test();
+
+$collection = collect([
+    ['product_id' => 'prod-100', 'name' => 'desk'],
+    ['product_id' => 'prod-200', 'name' => 'chair'],
+]);
+
+//$keyed = $collection->keyBy('product_id');
+//var_dump($keyed);
+$keyed = $collection->keyBy('product_id')->all();
+var_dump($keyed);
+
+
+
+
+
+
+
+
+
+
+
+
+//$collection = collect([1, 2, 3]);
+//var_dump($collection->keyBy());
+
+
+
+
+
+
+
+//function test()
+//{
+//    static $count = 0;
+//
+//    $count++;
+//    echo $count . PHP_EOL;
+//    if ($count < 10) {
+//        test();
+//    }
+//    $count--;
+//    echo 'minus ' . $count . PHP_EOL;
+//}
+//test();
+
+
+
+

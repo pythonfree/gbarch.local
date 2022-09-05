55521b23c045fef6f65f726c6a94ae819fc59c32
shahov.e <shahov.e@oneway.su>
2022-09-05 10:54:10 +0300

    Vue filters

--- www/Test/Vue/Gosha/main.js
+++ www/Test/Vue/Gosha/main.js
@@ -1,12 +1,71 @@
+
+Vue.filter('capitalize', function (value) {
+    if (!value) return '';
+    value = value.toString();
+    return value.replace(/\b\w/g, function (l) {
+        return l.toUpperCase();
+    })
+});
+
 new Vue({
     el: "#app",
     data: {
-        title: "Hello World!",
-        styleCSS: '',
+        message: 'Hello, world, hello.',
+        show: true,
+        cars: [
+            {model: 'model1', speed: 1},
+            {model: 'model2', speed: 2},
+            {model: 'model3', speed: 3},
+            {model: 'model4', speed: 4},
+        ]
     },
-    methods: {
-        changeText() {
-            this.title = "Текс от функции changeText()";
+    computed: {
+        showMess() {
+            return this.message.toUpperCase();
+        }
+    },
+    filters: {
+        lowerCase(value) {
+            return value.toLowerCase();
         }
     }
-});
+})
+
+
+// new Vue({
+//     el: "#app",
+//     data: {
+//         value: 1,
+//     },
+//     methods: {
+//         increment(value)
+//         {
+//             this.value = value;
+//             if (value == 24) {
+//                 console.log(`this value is ${value}`);
+//                 console.log("sdfsdf");
+//                 // alert('sdfsdf');
+//             }
+//         }
+//     },
+//     computed: {
+//         doubleValue()
+//         {
+//             return this.value * 2;
+//         }
+//     }
+// });
+
+
+// new Vue({
+//     el: "#app",
+//     data: {
+//         title: "Hello World!",
+//         styleCSS: '',
+//     },
+//     methods: {
+//         changeText() {
+//             this.title = "Текс от функции changeText()";
+//         }
+//     }
+// });

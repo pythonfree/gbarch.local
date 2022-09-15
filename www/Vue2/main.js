973895fc480c1b5926578f3242070e18ec6523a6
shahov.e <shahov.e@oneway.su>
2022-09-15 11:05:11 +0300

    Vue 3

--- www/Vue2/main.js
+++ www/Vue2/main.js
@@ -1,9 +1,15 @@
 var app = new Vue({
     el: '#app',
     data: {
+        url: '',
+        cleanUrl: '',
         count: 0,
     },
     methods: {
+        cleanerUrl: function () {
+            this.url = "https://ya.ru/"
+            this.cleanUrl = this.url.replace(/^https?:\/\//, '').replace(/\/$/, '')
+        },
         countUp: function () {
             this.count += 1
         },
